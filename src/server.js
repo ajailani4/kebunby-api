0b50ebe9db8f688c7501a5bffb61f4596f93eba4
Ahmad Jailani <ahmadjailani604@gmail.com>
2021-11-26 21:08:50 +0700

    Add Hapijs framework

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log('Hello');
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server is running on ${server.info.uri}`);
+};
+
+init();
